718b7999bf11aac6cc048871ccacd8c4457df764
Juntao Qiu <juntao.qiu@gmail.com>
2017-06-07 21:04:31 +0800

    todo component

---
+++ js/todo-component.js
@@ -0,0 +1,20 @@
+Vue.component('todo', {
+  template: '<li><slot></slot></li>'
+})
+
+Vue.component('todo-list', {
+  template: '<ul><todo v-for="todo in todos">{{todo.desc}}</todo></ul>',
+  data: function() {
+    return {
+      todos: [
+        {desc: "hello"},
+        {desc: "world"}
+      ]
+    }
+  }
+})
+
+
+var app = new Vue({
+  el: '#root'
+})
