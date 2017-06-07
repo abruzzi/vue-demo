403f2c9c5f1dc500946041de0a2ae08611d18439
Juntao Qiu <juntao.qiu@gmail.com>
2017-06-07 21:37:56 +0800

    add model component

---
+++ js/model.js
@@ -0,0 +1,20 @@
+Vue.component('model', {
+  template: `
+  <div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
+    <button class="modal-close" @click="$emit('close')"></button>
+  </div>
+  `
+});
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    showModel: false
+  }
+});
